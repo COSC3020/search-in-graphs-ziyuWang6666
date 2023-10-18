8a8b1199a60503ba7d70a9df51daf12d9713b0c5
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-10-18 17:51:13 -0600

    Create code.test.js
    create test.js

---
+++ code.test.js
@@ -0,0 +1,23 @@
+mconst fs = require('fs');
+const jsc = require('jsverify');
+
+eval(fs.readFileSync('code.js')+'');
+
+const test =
+    jsc.forall("array (pair nat nat)", function(edges) {
+        var max = edges.reduce(function(a, b) { return Math.max(a, Math.max(b[0], b[1])); }, 0);
+        var list = [];
+        for(var i = 0; i <= max; i++) {
+            list[i] = [];
+            for(var j = 0; j < edges.length; j++) {
+                if(edges[j][0] == i) list[i].push(edges[j][1]);
+            }
+            list[i].sort(function(a, b) { return a - b; });
+            list[i] = [...new Set(list[i])];
+        }
+        // console.log(list, '  test');
+        // to do some code to figure out path and then match with depthFirstSearch function
+        return JSON.stringify(list) == JSON.stringify(depthFirstSearch(list));
+    })
+
+jsc.assert(test, { tests: 1000 });
