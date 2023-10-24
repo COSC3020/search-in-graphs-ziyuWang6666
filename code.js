f41042731563ef9e8b366d850303044078860f81
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-10-24 17:40:40 -0600

    Update code.js
    change whole code.js file

--- code.js
+++ code.js
@@ -1,56 +1,24 @@
-let smallFlightMap = [
-    ["ATL", "DC"],
-    ["DC", "NYC"],
-    ["ATL", "DAL"],
-    ["DAL", "DC"]
-]
-const toAdjacencyList = function(edgeList) {
-    const adjacencyList = {};
-    for (let edge of edgeList) {
-      const src = edge[0];
-      const dest = edge[1];
-      if (adjacencyList[src] != undefined) {
-          adjacencyList[src].push(dest);
-      } else {
-          adjacencyList[src] = [dest];
-      }
-    }
-    return adjacencyList;
-}
-// find the path to get from startNode (source) to targetNode (dest)
-function depthFirstSearch(graph, startNode, targetNode) {
-    const graphList = toAdjacencyList(graph);
+  function depthFirstSearch(graph, startNode, targetNode) {
+    const stack = [startNode];
     const visited = new Set();
-    const stack = [];
-    stack.push(startNode);
-    const cameFrom = {};// keep track of where you came from
-    cameFrom[startNode] = undefined;//initialization
+    const paths = new Array(graph.length).fill([]);
+  
     while (stack.length > 0) {
-        const currentCity = stack.pop();
-        // console.log(currentCity);
-        const adjacentCities = graphList[currentCity] || [];//it is returning an empty array if graphList[currentCity] is undefined.
-        if (currentCity === targetNode) {
-            const pathFromSrcToDest = [];
-            pathFromSrcToDest.unshift(targetNode);
-            let previousCity = cameFrom[targetNode];
-                while (previousCity != undefined) {
-                    pathFromSrcToDest.unshift(previousCity);
-                    previousCity = cameFrom[previousCity];
-                }
-                return pathFromSrcToDest;
-        }
-        for (nextCity of adjacentCities) {//iterate children nodes
-            if (!visited.has(nextCity)) {
-                cameFrom[nextCity] = currentCity;
-                stack.push(nextCity);
-                visited.add(nextCity);
+      const vertex = stack.pop();
+      visited.add(vertex);
+  
+        for (const neighbor of graph[vertex]) {
+          if (!visited.has(neighbor)) {
+            stack.push(neighbor);
+            paths[neighbor] = paths[vertex].concat(neighbor);
+            if (neighbor === targetNode) {
+              // Include the source vertex in the path
+              return [startNode, ...paths[neighbor]];
             }
+          }
         }
     }
-    return [];
-}
-console.log(depthFirstSearch(smallFlightMap, "ATL", "NYC"))
-
-// I will add test.js later because I don't know how to write it right now.
+    return []
+  }
 // get whole reference from below website:
-// https://javascript.plainenglish.io/find-path-depth-first-search-28d27bba8ae0
+// https://stackoverflow.com/questions/12864004/tracing-and-returning-a-path-in-depth-first-search
