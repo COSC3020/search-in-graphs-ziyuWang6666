9fea5cbe56a21be87dd2ae0f2ac9706f6e423a26
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-05 20:36:28 -0700

    Update code.test.js
    change test.js

--- code.test.js
+++ code.test.js
@@ -1,23 +1,36 @@
 const fs = require('fs');
-const jsc = require('jsverify');
+const assert = require('assert');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js').toString());
 
-const test =
-    jsc.forall("array (pair nat nat)", function(edges) {
-        var max = edges.reduce(function(a, b) { return Math.max(a, Math.max(b[0], b[1])); }, 0);
-        var list = [];
-        for(var i = 0; i <= max; i++) {
-            list[i] = [];
-            for(var j = 0; j < edges.length; j++) {
-                if(edges[j][0] == i) list[i].push(edges[j][1]);
-            }
-            list[i].sort(function(a, b) { return a - b; });
-            list[i] = [...new Set(list[i])];
-        }
-        // console.log(list, '  test');
-        // to do some code to figure out path and then match with depthFirstSearch function
-        return JSON.stringify(list) == JSON.stringify(depthFirstSearch(list));
-    })
+// case one
+var graph1 = [];
+assert(JSON.stringify(depthFirstSearch(graph1, 0, 1)) == JSON.stringify([]));
 
-jsc.assert(test, { tests: 1000 });
+
+// case two
+var graph2 = [0];
+assert(JSON.stringify(depthFirstSearch(graph2, 0, 1)) == JSON.stringify([0]));
+
+
+// case three
+var graph3 = [[]];
+assert(JSON.stringify(depthFirstSearch(graph3, 0, 1)) == JSON.stringify([[]]));
+
+// case four
+var graph4 = [
+    [1,2],
+    [0,3,4],
+    [0,5],
+    [1],
+    [1,5],
+    [2,4]];
+assert(JSON.stringify(depthFirstSearch(graph4, 0, 5)) == JSON.stringify([ 0, 2, 5 ]));
+assert(JSON.stringify(depthFirstSearch(graph4, 1, 2)) == JSON.stringify([ 1, 4, 5, 2 ]));
+assert(JSON.stringify(depthFirstSearch(graph4, 0, 6)) == JSON.stringify([]));
+assert(JSON.stringify(depthFirstSearch(graph4, 0, 2)) == JSON.stringify([ 0, 2 ]));
+
+// case five
+var graph5 = [[-1]];
+assert(JSON.stringify(depthFirstSearch(graph5, 0, 1)) == JSON.stringify([[-1]]));
+assert(JSON.stringify(depthFirstSearch(graph5, 0, 0)) == JSON.stringify([[-1]]));
