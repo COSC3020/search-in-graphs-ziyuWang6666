895793cbb7ffb1c83b6b4d0ed2f4c55d2de7bc49
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-06 12:22:22 -0700

    Update code.js
    remove repeat code

--- code.js
+++ code.js
@@ -12,28 +12,7 @@ function depthFirstSearch(graph, startNode, targetNode) {
     visited.add(vertex);
 
       for (const neighbor of graph[vertex]) {
-        if (!visited.has(neighbor) && neighbor >= 0) {
-          stack.push(neighbor);
-          paths[neighbor] = paths[vertex].concat([neighbor]);
-          if (neighbor === targetNode) {
-            // Include the source vertex in the path
-            return [startNode, ...paths[neighbor]];
-          }
-        }
-      }
-  }
-  return []
-}
-  const stack = [startNode];
-  const visited = new Set();
-  const paths = new Array(graph.length).fill([]);
-
-  while (stack.length > 0) {
-    const vertex = stack.pop();
-    visited.add(vertex);
-
-      for (const neighbor of graph[vertex]) {
-        if (!visited.has(neighbor)) {
+        if (!visited.has(neighbor) && neighbor > 0) {
           stack.push(neighbor);
           paths[neighbor] = paths[vertex].concat([neighbor]);
           if (neighbor === targetNode) {
