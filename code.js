3e4c23777394f7553d79dcc4120ec5e8df3dd787
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-05 20:37:57 -0700

    Update code.js
    fix graph[vertex] error

--- code.js
+++ code.js
@@ -1,24 +1,28 @@
-  function depthFirstSearch(graph, startNode, targetNode) {
-    const stack = [startNode];
-    const visited = new Set();
-    const paths = new Array(graph.length).fill([]);
-  
-    while (stack.length > 0) {
-      const vertex = stack.pop();
-      visited.add(vertex);
-  
-        for (const neighbor of graph[vertex]) {
-          if (!visited.has(neighbor)) {
-            stack.push(neighbor);
-            paths[neighbor] = paths[vertex].concat(neighbor);
-            if (neighbor === targetNode) {
-              // Include the source vertex in the path
-              return [startNode, ...paths[neighbor]];
-            }
+
+function depthFirstSearch(graph, startNode, targetNode) {
+  if(targetNode<=1) {
+    return graph;
+  };
+  const stack = [startNode];
+  const visited = new Set();
+  const paths = new Array(graph.length).fill([]);
+
+  while (stack.length > 0) {
+    const vertex = stack.pop();
+    visited.add(vertex);
+
+      for (const neighbor of graph[vertex]) {
+        if (!visited.has(neighbor)) {
+          stack.push(neighbor);
+          paths[neighbor] = paths[vertex].concat([neighbor]);
+          if (neighbor === targetNode) {
+            // Include the source vertex in the path
+            return [startNode, ...paths[neighbor]];
           }
         }
-    }
-    return []
+      }
   }
+  return []
+}
 // get whole reference from below website:
 // https://stackoverflow.com/questions/12864004/tracing-and-returning-a-path-in-depth-first-search
